fab288a5a476595f273bec2b3eca68df45562310
P4tt4te <edward.brunetiere@gmail.com>
2025-01-22 00:51:18 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # poc-opencv
+This is a proof of concept project to demonstrate the use of OpenCV with C++.
---
+++ poc-opencv.cpp
@@ -0,0 +1,13 @@
+﻿// poc-opencv.cpp : Defines the entry point for the application.
+
+#include "poc-opencv.h"
+#include <fmt/core.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main()
+{
+	fmt::print("Hello, {}!\n", "world");
+	return 0;
+}
